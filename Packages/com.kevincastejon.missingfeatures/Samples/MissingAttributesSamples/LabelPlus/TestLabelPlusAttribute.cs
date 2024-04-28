598b9f0861df0df4e5957a6cdd6f50ca505b03a2
kevincastejon <contact@kevincastejon.fr>
2024-04-28 17:01:32 +0200

    fixed assets dependencies for Samples

--- Packages/com.kevincastejon.missingfeatures/Samples/MissingAttributesSamples/LabelPlus/TestLabelPlusAttribute.cs
+++ Packages/com.kevincastejon.missingfeatures/Samples/MissingAttributesSamples/LabelPlus/TestLabelPlusAttribute.cs
@@ -7,9 +7,9 @@ namespace KevinCastejon.MissingFeatures.MissingAttributesSamples
 {
     public class TestLabelPlusAttribute : MonoBehaviour
     {
-        [LabelPlus("Assets/Samples/MissingAttributesSamples/Shared/Icons/greencrossicon.png", "Health Points", (int)LabelPlusColor.green)]
+        [LabelPlus("Assets/Samples/MissingFeatures/1.0.0/MissingAttributesSamples/Shared/Icons/greencrossicon.png", "Health Points", (int)LabelPlusColor.green)]
         [SerializeField] private int _healthPoints;
-        [LabelPlus("Assets/Samples/MissingAttributesSamples/Shared/Icons/atk.png", "Damages", new float[] { 1f, 0f, 0f, 1f })]
+        [LabelPlus("Assets/Samples/MissingFeatures/1.0.0/MissingAttributesSamples/Shared/Icons/atk.png", "Damages", new float[] { 1f, 0f, 0f, 1f })]
         [SerializeField] private int _damages;
     }
 }
