6678cd2be420e836b8e10f0fa27321350cfaec11
kevincastejon <contact@kevincastejon.fr>
2024-04-28 17:49:00 +0200

    fixed icons paths on attributes demos

--- Samples/MissingAttributesSamples/HeaderPlus/TestHeaderPlusAttribute.cs
+++ Samples/MissingAttributesSamples/HeaderPlus/TestHeaderPlusAttribute.cs
@@ -5,10 +5,10 @@ namespace KevinCastejon.MissingFeatures.MissingAttributesSamples
 {
     public class TestHeaderPlusAttribute : MonoBehaviour
     {
-        [HeaderPlus("Assets/Samples/Missing Features/1.0.0/MissingAttributesSamples/Shared/Icons/greencrossicon.png", "Health", (int)HeaderPlusColor.green)]
+        [HeaderPlus("Assets/Samples/com.kevincastejon.missingfeatures/1.0.0/MissingAttributesSamples/Shared/Icons/greencrossicon.png", "Health", (int)HeaderPlusColor.green)]
         [SerializeField] private int _currentHealth;
         [SerializeField] private int _maxHealth;
-        [HeaderPlus("Assets/Samples/Missing Features/1.0.0/MissingAttributesSamples/Shared/Icons/atk.png", "Attack", (int)HeaderPlusColor.red)]
+        [HeaderPlus("Assets/Samples/com.kevincastejon.missingfeatures/1.0.0/MissingAttributesSamples/Shared/Icons/atk.png", "Attack", (int)HeaderPlusColor.red)]
         [SerializeField] private int _damagePoints;
         [SerializeField] private int _attackSpeed;
     }
