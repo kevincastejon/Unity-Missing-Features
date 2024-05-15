469e5e14efd605b7bb2bfb8bf9a85954eed1fa3c
kevincastejon <contact@kevincastejon.fr>
2024-05-15 19:55:36 +0200

    Update ReadOnlyOnPlayDrawer.cs

--- Packages/com.kevincastejon.missingfeatures/Editor/MissingAttributes/ReadOnlyOnPlayAttribute/ReadOnlyOnPlayDrawer.cs
+++ Packages/com.kevincastejon.missingfeatures/Editor/MissingAttributes/ReadOnlyOnPlayAttribute/ReadOnlyOnPlayDrawer.cs
@@ -8,7 +8,7 @@ namespace KevinCastejon.MissingFeatures.MissingAttributes
     {
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            return EditorGUI.GetPropertyHeight(property, label);
+            return EditorGUI.GetPropertyHeight(property, label, true);
         }
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
