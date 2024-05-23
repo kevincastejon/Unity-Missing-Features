3bfe3f63a3288d1ce9bd947b31bb16310f3fe647
kevincastejon <contact@kevincastejon.fr>
2024-05-23 21:45:54 +0200

    added TransformTeleporter component

---
+++ Runtime/MissingComponents/TransformTeleporter/TransformTeleporter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformTeleporter : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Transform _to;
+    [SerializeField] private bool _usePosition = true;
+    [SerializeField] private bool _useRotation = true;
+    [SerializeField] private bool _useScale = true;
+
+    public void Teleport()
+    {
+        if (_usePosition)
+        {
+            _target.position = _to.position;
+        }
+        if (_useRotation)
+        {
+            _target.rotation = _to.rotation;
+        }
+        if (_useScale)
+        {
+            _target.localScale = _to.localScale;
+        }
+    }
+}
