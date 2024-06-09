cb4dfd0b141c9d16d185bd49af69e208bdd4da2a
kevincastejon <castejonkevin@gmail.com>
2024-06-09 02:37:00 +0200

    reworked TransformData class

--- Runtime/MissingComponents/TransformTeleporter/TransformTeleporter.cs
+++ Runtime/MissingComponents/TransformTeleporter/TransformTeleporter.cs
@@ -2,30 +2,93 @@ using KevinCastejon.MissingFeatures.SharedUtils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace KevinCastejon.MissingFeatures.MissingComponents
 {
     public class TransformTeleporter : MonoBehaviour
     {
+        [Tooltip("The target Transform.")]
         [SerializeField] private Transform _target;
-        [SerializeField] private TransformData _to;
+        [Tooltip("The Transform data to use for teleportation.")]
+        [SerializeField] private TransformData _destination;
+        [Tooltip("Will use position value for teleporting.")]
         [SerializeField] private bool _usePosition = true;
+        [Tooltip("Will use rotation value for teleporting.")]
         [SerializeField] private bool _useRotation = true;
+        [Tooltip("Will use scale value for teleporting.")]
         [SerializeField] private bool _useScale = true;
 
-        public void Teleport()
+        /// <summary>
+        /// The target Transform.
+        /// </summary>
+        public Transform Target { get => _target; set => _target = value; }
+        /// <summary>
+        /// The Transform data to use for teleportation.
+        /// </summary>
+        public TransformData Destination { get => _destination; set => _destination = value; }
+        /// <summary>
+        /// Will use position value for teleporting.
+        /// </summary>
+        public bool UsePosition { get => _usePosition; set => _usePosition = value; }
+        /// <summary>
+        /// Will use rotation value for teleporting.
+        /// </summary>
+        public bool UseRotation { get => _useRotation; set => _useRotation = value; }
+        /// <summary>
+        /// Will use scale value for teleporting.
+        /// </summary>
+        public bool UseScale { get => _useScale; set => _useScale = value; }
+        /// <summary>
+        /// Set the destination values to the local position rotation and scale of the specified Transform component.
+        /// </summary>
+        /// <param name="transform">The Transform component used to feed the data from.</param>
+        public void SetDestinationLocalFromTransform(Transform transform)
+        {
+            _destination.SetTransformDataLocalFromTransform(transform);
+        }
+        /// <summary>
+        /// Set the destination values to the global position rotation and scale of the specified Transform component.
+        /// </summary>
+        /// <param name="transform">The Transform component used to feed the data from.</param>
+        public void SetDestinationGlobalFromTransform(Transform transform)
+        {
+            _destination.SetTransformDataGlobalFromTransform(transform);
+        }
+        /// <summary>
+        /// Sets the Transform world coordinates values to the destination ones.
+        /// </summary>
+        public void TeleportGlobal()
         {
             if (_usePosition)
             {
-                _target.position = _to.Position;
+                _target.position = _destination.Position;
             }
             if (_useRotation)
             {
-                _target.rotation = _to.Rotation;
+                _target.rotation = _destination.Rotation;
             }
             if (_useScale)
             {
-                _target.localScale = _to.Scale;
+                _target.localScale = _destination.Scale;
+            }
+        }
+        /// <summary>
+        /// Sets the Transform local coordinates values to the destination ones.
+        /// </summary>
+        public void TeleportLocal()
+        {
+            if (_usePosition)
+            {
+                _target.localPosition = _destination.Position;
+            }
+            if (_useRotation)
+            {
+                _target.localRotation = _destination.Rotation;
+            }
+            if (_useScale)
+            {
+                _target.localScale = _destination.Scale;
             }
         }
     }
--- Runtime/SharedUtils/TransformData.cs
+++ Runtime/SharedUtils/TransformData.cs
@@ -10,20 +10,10 @@ namespace KevinCastejon.MissingFeatures.SharedUtils
     [Serializable]
     public class TransformData
     {
-        [SerializeField] private Transform _reference = null;
-        [SerializeField] private Transform _parent = null;
         [SerializeField] private Vector3 _position = Vector3.zero;
         [SerializeField] private Quaternion _rotation = Quaternion.identity;
         [SerializeField] private Vector3 _scale = Vector3.one;
 
-        /// <summary>
-        /// Optional Transform reference that will be used to feed the data
-        /// </summary>
-        public Transform Reference { get => _reference; set => _reference = value; }
-        /// <summary>
-        /// An optional reference to a parent Transform
-        /// </summary>
-        public Transform Parent { get => _parent; set => _parent = value; }
         /// <summary>
         /// The position data
         /// </summary>
@@ -36,5 +26,25 @@ namespace KevinCastejon.MissingFeatures.SharedUtils
         /// The scale data
         /// </summary>
         public Vector3 Scale { get => _scale; set => _scale = value; }
+        /// <summary>
+        /// Set the values to the local position rotation and scale of the specified Transform component.
+        /// </summary>
+        /// <param name="transform">The Transform component used to feed the data from.</param>
+        public void SetTransformDataLocalFromTransform(Transform transform)
+        {
+            _position = transform.localPosition;
+            _rotation = transform.localRotation;
+            _scale = transform.localScale;
+        }
+        /// <summary>
+        /// Set the values to the local position rotation and scale of the specified Transform component.
+        /// </summary>
+        /// <param name="transform">The Transform component used to feed the data from.</param>
+        public void SetTransformDataGlobalFromTransform(Transform transform)
+        {
+            _position = transform.position;
+            _rotation = transform.rotation;
+            _scale = transform.localScale;
+        }
     }
 }
