66feab50ce42ebdca128d636ec9b315e30cb51c9
kevincastejon <castejonkevin@gmail.com>
2024-07-16 19:36:24 +0200

    added DontDestroyOnLoadComponent docs

--- Runtime/MissingComponents/DontDestroyOnLoadComponent/DontDestroyOnLoadComponent.cs
+++ Runtime/MissingComponents/DontDestroyOnLoadComponent/DontDestroyOnLoadComponent.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// A component that places its related gameobject into the DontDestroyOnLoad scene on Start.
+/// </summary>
 public class DontDestroyOnLoadComponent : MonoBehaviour
 {
     void Start()
